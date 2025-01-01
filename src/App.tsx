12d2eb44cf0c9fef756363642bd3a6d3198faab6
ianpatricck <ianpatrick499@gmail.com>
2025-01-01 07:38:35 -0300

    :recycle: importing the ThreeAmbient component

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,7 @@
-import {
-  Environment,
-  OrbitControls,
-  PerspectiveCamera,
-} from "@react-three/drei";
+import { Environment, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import AppModule from "./App.module.css";
-import Table from "./3d/Table";
-import Chair from "./3d/Chair";
-import Monitor from "./3d/Monitor";
-import Keyboard from "./3d/Keyboard";
-import Mouse from "./3d/Mouse";
-import Computer from "./3d/Computer";
-import Scenario from "./3d/Scenario";
-
-const ThreeCenary = () => {
-  return (
-    <group scale={2}>
-      <Scenario
-        position={{
-          x: 0,
-          y: 0,
-          z: 0,
-        }}
-      />
-      <Table
-        position={{
-          x: 1.8,
-          y: 0.02,
-          z: -2.7,
-        }}
-        scale={0.3}
-        rotation={{ x: 0, y: -1.6, z: 0 }}
-      />
-      <Chair
-        position={{
-          x: 1.8,
-          y: 0.02,
-          z: -1.4,
-        }}
-        scale={0.6}
-        rotation={{ x: 0, y: 1.9, z: 0 }}
-      />
-
-      <Monitor
-        position={{
-          x: 1.8,
-          y: 0.6,
-          z: -2.7,
-        }}
-        scale={0.6}
-        rotation={{ x: 0, y: 1.2, z: 0 }}
-      />
-      <Keyboard
-        position={{
-          x: 1.7,
-          y: 0.62,
-          z: -2.4,
-        }}
-        scale={0.6}
-        rotation={{ x: 0, y: 1.5, z: 0 }}
-      />
-      <Mouse
-        position={{
-          x: 2.2,
-          y: 0.62,
-          z: -2.4,
-        }}
-        scale={0.07}
-        rotation={{ x: 0, y: 0, z: 0 }}
-      />
-      <Computer
-        position={{
-          x: 2.5,
-          y: 0.62,
-          z: -2.6,
-        }}
-        scale={0.3}
-        rotation={{ x: 0, y: -1.6, z: 0 }}
-      />
-    </group>
-  );
-};
+import ThreeAmbient from "./3d/Ambient";
 
 const Scene = () => {
   return (
@@ -88,7 +9,7 @@ const Scene = () => {
       {/* Main camera */}
       <PerspectiveCamera
         makeDefault
-        position={[-1.5, 3, -1.5]}
+        position={[-1.5, 4, -2.6]}
         rotation={[-0.7, -1, -0.6]}
       />
 
@@ -96,12 +17,10 @@ const Scene = () => {
       <ambientLight intensity={1} />
       <Environment preset="sunset" />
 
-      {/* Main cenary */}
-      <ThreeCenary />
+      {/* Main ambient */}
+      <ThreeAmbient />
 
       {/* Development settings */}
-
-      <OrbitControls />
       <axesHelper args={[100]} />
     </>
   );
