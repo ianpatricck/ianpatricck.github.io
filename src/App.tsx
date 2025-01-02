59b8fea70926c4f7d463cc303563ad1329d97042
ianpatricck <ianpatrick499@gmail.com>
2025-01-02 16:38:09 -0300

    :art: Sidebar component was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Canvas } from "@react-three/fiber";
 import AppModule from "./App.module.css";
 import { Suspense } from "react";
 import Scene from "./3d/Scene";
+import Sidebar from "./layouts/Sidebar/Sidebar";
 
 export default function App() {
   return (
     <Suspense fallback={null}>
       <div className={AppModule.wrapper}>
+        <Sidebar />
+
         <Canvas>
           <Scene />
         </Canvas>
