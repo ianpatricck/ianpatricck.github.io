344ae4117fa9d6db57d3d1313dda89faf4435cea
ianpatricck <ianpatrick499@gmail.com>
2025-01-06 22:36:49 -0300

    :recycle: import sidebar folder component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import AppModule from "./App.module.css";
 import { Suspense } from "react";
 import Scene from "./3d/Scene";
-import Sidebar from "./layouts/Sidebar/Sidebar";
+import Sidebar from "./layouts/Sidebar";
 
 export default function App() {
   return (
