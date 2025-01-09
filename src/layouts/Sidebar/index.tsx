794b17af72a8c1c3530ac091d1ebb311e3b4213f
ianpatricck <ianpatrick499@gmail.com>
2025-01-09 20:19:30 -0300

    imported new decomposed components

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -1,10 +1,14 @@
 import AboutMe from "../../components/AboutMe";
+import Presentation from "../../components/Presentation";
+import Services from "../../components/Services";
 import { sidebar } from "./Sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <aside className={sidebar}>
+      <Presentation />
       <AboutMe />
+      <Services />
     </aside>
   );
 }
