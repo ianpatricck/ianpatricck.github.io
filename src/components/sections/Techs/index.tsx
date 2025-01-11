183972cdbbae7151d326eb485bad8b7b78553c2f
ianpatricck <ianpatrick499@gmail.com>
2025-01-11 20:18:53 -0300

    created Techs component

---
+++ src/components/sections/Techs/index.tsx
@@ -0,0 +1,10 @@
+import Topic from "../../elements/Topic";
+import Style from "./Techs.module.css";
+
+export default function Techs() {
+  return (
+    <div className={Style.techs}>
+      <Topic>Algumas habilidades</Topic>
+    </div>
+  );
+}
