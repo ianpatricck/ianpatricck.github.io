76370b240fdd1a849a84ce4a262c91d66df7d8a0
ianpatricck <ianpatrick499@gmail.com>
2025-01-11 20:03:27 -0300

    fixed imports

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -1,6 +1,6 @@
-import AboutMe from "../../components/AboutMe";
-import Presentation from "../../components/Presentation";
-import Services from "../../components/Services";
+import AboutMe from "../../components/sections/AboutMe";
+import Presentation from "../../components/sections/Presentation";
+import Services from "../../components/sections/Services";
 import { sidebar } from "./Sidebar.module.css";
 
 export default function Sidebar() {
