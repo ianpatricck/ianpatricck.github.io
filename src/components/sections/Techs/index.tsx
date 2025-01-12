11192ef246da4fbc8b214706ae4b60ec44c564e9
ianpatricck <ianpatrick499@gmail.com>
2025-01-11 22:44:46 -0300

    implemented skills in Techs component

--- src/components/sections/Techs/index.tsx
+++ src/components/sections/Techs/index.tsx
@@ -2,9 +2,69 @@ import Topic from "../../elements/Topic";
 import Style from "./Techs.module.css";
 
 export default function Techs() {
+  const mystack: string[] = [
+    "c",
+    "cpp",
+    "php",
+    "nodejs",
+    "express",
+    "nest",
+    "graphql",
+    "laravel",
+    "symfony",
+    "nginx",
+    "npm",
+    "redis",
+    "mongodb",
+    "mysql",
+    "sqlite",
+    "postgres",
+    "rabbitmq",
+    "heroku",
+    "aws",
+    "kubernetes",
+    "docker",
+    "prisma",
+    "sequelize",
+    "vercel",
+    "firebase",
+    "jest",
+    "githubactions",
+    "html",
+    "js",
+    "ts",
+    "css",
+    "bootstrap",
+    "tailwindcss",
+    "sass",
+    "vite",
+    "styledcomponents",
+    "materialui",
+    "react",
+    "webpack",
+    "babel",
+    "redux",
+    "figma",
+    "jest",
+    "cypress",
+    "electron",
+    "threejs",
+    "vitest",
+  ];
+
   return (
     <div className={Style.techs}>
-      <Topic>Algumas habilidades</Topic>
+      <Topic>Conhecimentos técnicos</Topic>
+
+      <div className={Style.techs_wrapper}>
+        {mystack.map((tech, index) => (
+          <img
+            key={index}
+            src={"https://skillicons.dev/icons?i=" + tech}
+            alt={tech}
+          />
+        ))}
+      </div>
     </div>
   );
 }
