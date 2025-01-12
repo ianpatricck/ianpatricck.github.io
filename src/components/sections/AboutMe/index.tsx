6741b30f245e253cb49ec58438d9abb6e07784e2
ianpatricck <ianpatrick499@gmail.com>
2025-01-11 22:42:21 -0300

    added the Text component

--- src/components/sections/AboutMe/index.tsx
+++ src/components/sections/AboutMe/index.tsx
@@ -1,3 +1,4 @@
+import Text from "../../elements/Text";
 import Topic from "../../elements/Topic";
 import Style from "./AboutMe.module.css";
 
@@ -6,12 +7,12 @@ export default function AboutMe() {
     <div className={Style.about}>
       <Topic>Sobre mim</Topic>
 
-      <p className={Style.about__description}>
+      <Text>
         Com mais de 3 anos de experiência em desenvolvimento de web, sou
         apaixonado por criar soluções robustas, escaláveis e acima de tudo,
         resolver problemas. Desenvolvi e fiz parte de projetos que envolviam
         desde UI até banco de dados.
-      </p>
+      </Text>
     </div>
   );
 }
