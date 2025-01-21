a7c4c0f938c43e2d0eb5824c3a58da5869f2e214
ianpatricck <ianpatrick499@gmail.com>
2025-01-21 14:49:47 -0300

    rotate animation on hover object

--- src/3d/Objects/Laptop.tsx
+++ src/3d/Objects/Laptop.tsx
@@ -1,22 +1,53 @@
-import React from "react";
-import { useLoader } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Mesh } from "three";
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
+};
 
 type LaptopProps = {
-  position: { x: number; y: number; z: number };
+  position: Vector;
   scale: number;
-  rotation: { x: number; y: number; z: number };
+  rotation: Vector;
 };
 
 export default function Laptop({ position, scale, rotation }: LaptopProps) {
   const laptop = useLoader(GLTFLoader, "/models/laptop.glb");
+
+  const ref = useRef<Mesh>(null);
+
+  const [hovered, setHovered] = useState<boolean>(false);
+  const [currentScale, setCurrentScale] = useState<number>(scale);
+  const [currentRotation, setCurrentRotation] = useState<Vector>(rotation);
+
+  useFrame(({ clock }) => {
+    if (hovered) {
+      document.body.style.cursor = hovered ? "pointer" : "auto";
+      setCurrentRotation({
+        x: rotation.x,
+        y: clock.getElapsedTime(),
+        z: rotation.z,
+      });
+    } else {
+      setCurrentScale(scale);
+      setCurrentRotation({ x: rotation.x, y: currentRotation.y, z: rotation.z });
+    }
+  });
+
   return (
     <mesh
+      ref={ref}
       position={[position.x, position.y, position.z]}
-      scale={scale}
-      rotation={[rotation.x, rotation.y, rotation.z]}
+      scale={currentScale}
+      rotation={[currentRotation.x, currentRotation.y, currentRotation.z]}
+      onPointerOver={() => setHovered(true)}
+      onPointerLeave={() => setHovered(false)}
     >
-      <primitive object={laptop.scene} />;
+      <primitive object={laptop.scene} />
     </mesh>
   );
 }
