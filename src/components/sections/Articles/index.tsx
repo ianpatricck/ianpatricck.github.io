534024be477f0fc976312842e3566f53b808ed0b
ianpatricck <ianpatrick499@gmail.com>
2025-01-25 21:55:14 -0300

    animated article's component

--- src/components/sections/Articles/index.tsx
+++ src/components/sections/Articles/index.tsx
@@ -2,22 +2,53 @@ import ArticleCard from "../../elements/ArticleCard";
 import Topic from "../../elements/Topic";
 import Style from "./Articles.module.css";
 import { items } from "./data";
+import { motion } from "motion/react";
+
+/*
+ *  Componente de artigos
+ *
+ *  Exibe uma lista de artigos que o autor escreveu.
+ *
+ */
 
 export default function Articles() {
   return (
-    <div className={Style.articles}>
+    <motion.div
+      className={Style.articles}
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          ease: "easeIn",
+        },
+      }}
+    >
       <Topic>Artigos</Topic>
       <div className={Style.articles_container}>
         {items.map((article, key) => (
-          <ArticleCard
+          <motion.div
             key={key}
-            image={article.image}
-            title={article.title}
-            from={article.from}
-            url={article.url}
-          />
+            initial={{ x: -50 }}
+            animate={{
+              transition: {
+                duration: 1.5,
+                ease: "easeIn",
+              },
+            }}
+            whileInView={{ x: 0 }}
+          >
+            <ArticleCard
+              image={article.image}
+              title={article.title}
+              from={article.from}
+              url={article.url}
+            />
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
