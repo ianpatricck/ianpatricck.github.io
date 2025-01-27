62a3ca0722fa31670d0678a1b8ca86d9bc4582e2
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 20:44:04 -0300

    added comment on Text component

--- src/components/elements/Text/index.tsx
+++ src/components/elements/Text/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { ReactNode } from "react";
 import Style from "./Text.module.css";
 
-type TextProps = {
-  children: React.ReactNode;
-};
+/*
+ *  Componente de texto
+ *
+ *  Retorna um elemento parágrafo (p) estilizado.
+ *
+ */
 
-export default function Text({ children }: TextProps) {
+export default function Text({ children }: { children: ReactNode }) {
   return <p className={Style.text}>{children}</p>;
 }
