b5d90d1bd703ad3909f7e969e861279e591ee613
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 20:55:24 -0300

    Link component was commented

--- src/components/elements/Link/index.tsx
+++ src/components/elements/Link/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Style from "./Link.module.css";
 
+/*
+ *  Tipagem do Link
+ *
+ */
 type TextProps = {
   href: string;
   children: React.ReactNode;
 };
+
+/*
+ *  Componente de Link
+ *
+ *  Apenas cria um link personalizado e retorna o componente.
+ *
+ */
 
 export default function Link({ href, children }: TextProps) {
   return (
