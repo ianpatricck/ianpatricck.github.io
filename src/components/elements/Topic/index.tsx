8681e74624e5d08ab5e08f8c3f3b6b33c9022c31
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 20:44:14 -0300

    added comment on Topic component

--- src/components/elements/Topic/index.tsx
+++ src/components/elements/Topic/index.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
 import Style from "./Topic.module.css";
 
-type TopicProps = {
-  children: ReactNode;
-};
+/*
+ *  Componente de tópico
+ *
+ *  Retorna um elemento título (h1) personalizado.
+ *
+ */
 
-export default function Topic({ children }: TopicProps) {
+export default function Topic({ children }: { children: ReactNode }) {
   return (
     <h1 className={Style.topic}>
       <b>#</b> <span>{children}</span>
