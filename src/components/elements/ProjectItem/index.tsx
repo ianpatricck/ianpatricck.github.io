ca3925c013acf7d9968106f8fd4145cb83ca2acc
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 20:53:45 -0300

    commented ProjectItem component

--- src/components/elements/ProjectItem/index.tsx
+++ src/components/elements/ProjectItem/index.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import Style from "./ProjectItem.module.css";
 
+/*
+ *  Tipagem das propriedades do componente
+ *
+ */
+
 type ProjectItemProps = {
   image: string;
   children: React.ReactNode;
 };
 
+/*
+ *  Componente de um item de projeto
+ *
+ *  Representa o conteúdo inserido dentro do componente 'Projects'.
+ *
+ */
+
 export default function ProjectItem({ image, children }: ProjectItemProps) {
+  // imagem a ser exibida ao fundo do item
   const [background, setBackground] = useState<string>(`url(${image})`);
+
+  // constante que verifica se o conteúdo está visível na tela
   const [contentIsDisplayed, setContentIsDisplayed] = useState<boolean>(false);
 
+  // função chamada quando o cursor paira sobre o item
   function projectItemEnter() {
     const darkBackground = `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url(${image})`;
 
@@ -17,6 +33,7 @@ export default function ProjectItem({ image, children }: ProjectItemProps) {
     setContentIsDisplayed(true);
   }
 
+  // função chamada quando o cursor está fora do item
   function projectItemLeave() {
     setBackground(`url(${image})`);
     setContentIsDisplayed(false);
