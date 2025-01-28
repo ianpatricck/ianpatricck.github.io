fc09109b87a46cfad70b180eecf54ebc1ed790d3
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 22:07:21 -0300

    comments on Bookshelf component

--- src/3d/Objects/Bookshelf.tsx
+++ src/3d/Objects/Bookshelf.tsx
@@ -2,14 +2,55 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-type BookshelfProps = {
-  position: { x: number; y: number; z: number };
-  scale: number;
-  rotation: { x: number; y: number; z: number };
+/*
+ *  Vector Type
+ *
+ *  Tipo que representa um vetor em algumas propriedades
+ *  como 'position' ou 'rotation'.
+ *
+ */
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
 };
 
-export default function Bookshelf({ position, scale, rotation }: BookshelfProps) {
+/*
+ *  BookshelfProps
+ *
+ *  Tipo que as propriedades 'props' do componente Bookshelf recebem.
+ *
+ */
+
+type BookshelfProps = {
+  position: Vector;
+  scale: number;
+  rotation: Vector;
+};
+
+/*
+ *  Componente Bookshelf
+ *
+ *  Componente principal que representa um objeto 3d.
+ *
+ */
+
+export default function Bookshelf({
+  position,
+  scale,
+  rotation,
+}: BookshelfProps) {
+  // Constante que carrega o modelo do objeto (glb).
   const bookshelf = useLoader(GLTFLoader, "/models/bookshelf.glb");
+
+  /*
+   *  Retorno do mesh
+   *
+   *  Retorna uma malha e exibe o objeto importado dentro de <primitive>.
+   *
+   */
+
   return (
     <mesh
       position={[position.x, position.y, position.z]}
