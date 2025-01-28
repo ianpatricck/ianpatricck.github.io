634bd5f133f3656854940b7e0b77536d2785c47f
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 22:14:36 -0300

    commented the Computer component

--- src/3d/Objects/Computer.tsx
+++ src/3d/Objects/Computer.tsx
@@ -2,14 +2,51 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-type ComputerProps = {
-  position: { x: number; y: number; z: number };
-  scale?: number;
-  rotation: { x: number; y: number; z: number };
+/*
+ *  Vector Type
+ *
+ *  Tipo que representa um vetor em algumas propriedades
+ *  como 'position' ou 'rotation'.
+ *
+ */
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
 };
 
+/*
+ *  ComputerProps
+ *
+ *  Tipo que as propriedades 'props' do componente Computer recebem.
+ *
+ */
+
+type ComputerProps = {
+  position: Vector;
+  scale?: number;
+  rotation: Vector;
+};
+
+/*
+ *  Componente Computer
+ *
+ *  Componente principal que representa um objeto 3d.
+ *
+ */
+
 export default function Computer({ position, scale, rotation }: ComputerProps) {
+  // Constante que carrega o modelo do objeto (glb).
   const computer = useLoader(GLTFLoader, "/models/computer.glb");
+
+  /*
+   *  Retorno do mesh
+   *
+   *  Retorna uma malha e exibe o objeto importado dentro de <primitive>.
+   *
+   */
+
   return (
     <mesh
       position={[position.x, position.y, position.z]}
