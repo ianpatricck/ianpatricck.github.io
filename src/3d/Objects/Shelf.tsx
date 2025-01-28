484bcb9d82468da8d4965b054f61fb59f6733fae
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 22:29:26 -0300

    comments on Shelf object component

--- src/3d/Objects/Shelf.tsx
+++ src/3d/Objects/Shelf.tsx
@@ -2,14 +2,51 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-type ShelfProps = {
-  position: { x: number; y: number; z: number };
-  scale: number;
-  rotation: { x: number; y: number; z: number };
+/*
+ *  Vector Type
+ *
+ *  Tipo que representa um vetor em algumas propriedades
+ *  como 'position' ou 'rotation'.
+ *
+ */
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
 };
 
+/*
+ *  ShelfProps
+ *
+ *  Tipo que as propriedades 'props' do componente Shelf recebem.
+ *
+ */
+
+type ShelfProps = {
+  position: Vector;
+  scale: number;
+  rotation: Vector;
+};
+
+/*
+ *  Componente Shelf
+ *
+ *  Componente principal que representa um objeto 3d.
+ *
+ */
+
 export default function Shelf({ position, scale, rotation }: ShelfProps) {
+  // Constante que carrega o modelo do objeto (glb).
   const shelf = useLoader(GLTFLoader, "/models/shelf.glb");
+
+  /*
+   *  Retorno do mesh
+   *
+   *  Retorna uma malha e exibe o objeto importado dentro de <primitive>.
+   *
+   */
+
   return (
     <mesh
       position={[position.x, position.y, position.z]}
