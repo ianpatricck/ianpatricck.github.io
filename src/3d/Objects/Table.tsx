98ab02b42a548fd7f6c11ef2f4c97262d3fd1589
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 22:31:02 -0300

    comments on Table object component

--- src/3d/Objects/Table.tsx
+++ src/3d/Objects/Table.tsx
@@ -2,14 +2,51 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-type TableProps = {
-  position: { x: number; y: number; z: number };
-  scale: number;
-  rotation: { x: number; y: number; z: number };
+/*
+ *  Vector Type
+ *
+ *  Tipo que representa um vetor em algumas propriedades
+ *  como 'position' ou 'rotation'.
+ *
+ */
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
 };
 
+/*
+ *  TableProps
+ *
+ *  Tipo que as propriedades 'props' do componente Table recebem.
+ *
+ */
+
+type TableProps = {
+  position: Vector;
+  scale: number;
+  rotation: Vector;
+};
+
+/*
+ *  Componente Table
+ *
+ *  Componente principal que representa um objeto 3d.
+ *
+ */
+
 export default function Table({ position, scale, rotation }: TableProps) {
+  // Constante que carrega o modelo do objeto (glb).
   const table = useLoader(GLTFLoader, "/models/table.glb");
+
+  /*
+   *  Retorno do mesh
+   *
+   *  Retorna uma malha e exibe o objeto importado dentro de <primitive>.
+   *
+   */
+
   return (
     <mesh
       position={[position.x, position.y, position.z]}
