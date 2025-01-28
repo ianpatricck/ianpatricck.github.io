c9755a172b1d633b5c59a076a7e242cc8437fe3d
ianpatricck <ianpatrick499@gmail.com>
2025-01-27 22:23:10 -0300

    comments on Mouse component

--- src/3d/Objects/Mouse.tsx
+++ src/3d/Objects/Mouse.tsx
@@ -2,14 +2,51 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-type MouseProps = {
-  position: { x: number; y: number; z: number };
-  scale: number;
-  rotation: { x: number; y: number; z: number };
+/*
+ *  Vector Type
+ *
+ *  Tipo que representa um vetor em algumas propriedades
+ *  como 'position' ou 'rotation'.
+ *
+ */
+
+type Vector = {
+  x: number;
+  y: number;
+  z: number;
 };
 
+/*
+ *  MouseProps
+ *
+ *  Tipo que as propriedades 'props' do componente Mouse recebem.
+ *
+ */
+
+type MouseProps = {
+  position: Vector;
+  scale: number;
+  rotation: Vector;
+};
+
+/*
+ *  Componente Mouse
+ *
+ *  Componente principal que representa um objeto 3d.
+ *
+ */
+
 export default function Mouse({ position, scale, rotation }: MouseProps) {
+  // Constante que carrega o modelo do objeto (glb).
   const mouse = useLoader(GLTFLoader, "/models/mouse.glb");
+
+  /*
+   *  Retorno do mesh
+   *
+   *  Retorna uma malha e exibe o objeto importado dentro de <primitive>.
+   *
+   */
+
   return (
     <mesh
       position={[position.x, position.y, position.z]}
