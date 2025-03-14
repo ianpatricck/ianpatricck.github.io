89d4e9da079b433556d1f263017c25dfb67311e7
ianpatricck <ianpatrick499@gmail.com>
2025-03-14 17:20:09 -0300

    created an useScreenDimensions custom hook

---
+++ src/hooks/useScreenDimensions.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useScreenDimensions(): number[] {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [height, setHeight] = useState<number>(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  return [width, height];
+}
