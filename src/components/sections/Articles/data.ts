bce012ea136f4166853ac7173d5ea435b5e8035a
ianpatricck <ianpatrick499@gmail.com>
2025-03-17 23:07:12 -0300

    added +1 item on article data list

--- src/components/sections/Articles/data.ts
+++ src/components/sections/Articles/data.ts
@@ -1,6 +1,14 @@
 export const items = [
   {
     image:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*bdzFrD1qJDiug5AohbcXow.png",
+    title:
+      "Métodos Mágicos em PHP: Porque Programadores Também Precisam de Magia",
+    from: "Medium",
+    url: "https://medium.com/@ianpatricck/m%C3%A9todos-m%C3%A1gicos-em-php-porque-programadores-tamb%C3%A9m-precisam-de-magia-a5b4d38b439f",
+  },
+  {
+    image:
       "https://miro.medium.com/v2/resize:fit:720/format:webp/1*ZN7RHycTKUtHL5NC0ir_Ig.png",
     title: "React 19: Novas features e demais novidades",
     from: "Medium",
