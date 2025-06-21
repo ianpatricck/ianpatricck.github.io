9d2ce8ce8f11c2bb67135fb10c593f1b7b3dce4c
ianpatricck <ianpatrick499@gmail.com>
2025-06-21 10:35:05 -0300

    removed ModalProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/reset.css";
 import "./styles/main.css";
 import App from "./App";
-import { ModalProvider } from "./context/ModalContext";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <App />
   </React.StrictMode>,
 );
